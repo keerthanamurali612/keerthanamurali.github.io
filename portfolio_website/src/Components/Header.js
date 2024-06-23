d0228be52cec776a5c588064156dbf0f84b01c0f
keerthanamurali612 <keerthanamurali099@gmail.com>
2024-06-23 19:14:18 +0530

    header navigation

---
+++ portfolio_website/src/Components/Header.js
@@ -0,0 +1,34 @@
+import { Bars3Icon } from '@heroicons/react/24/solid'
+import { useState } from 'react'
+export default function Header(){
+  const [toggleMenu,setToggleMenu]=useState(false);
+
+
+    return <header className="flex justify-between px-5 py-5 bg-primary ">
+        <a className="font-bold text-black text-2xl  " href="#">Porfolio</a>
+
+        <nav className="hidden md:block" >
+            <ul className="flex text-white text-xl">
+              <li><a href="#">Home</a></li>
+              <li><a href="#">About</a></li>
+              <li><a href="#">Project</a></li>
+              <li><a href="#">Contact</a></li>
+            </ul>
+
+        </nav>
+        {/* mobile view */}
+       { toggleMenu&&  <nav className="block md:hidden " >
+            <ul className="flex flex-col text-white text-xl mobile-nav">
+              <li><a href="#">Home</a></li>
+              <li><a href="#">About</a></li>
+              <li><a href="#">Project</a></li>
+              <li><a href="#">Contact</a></li>
+            </ul>
+
+        </nav>
+        }
+        <button  onClick={()=>setToggleMenu(!toggleMenu)} className='block md:hidden'><Bars3Icon className='text-white h-5'/> </button>
+        
+   </header>
+    
+}
