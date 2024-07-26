9b56109d78eeef3c1e6e17765c3f6be0cb6a2bd3
keerthanamurali612 <keerthanamurali099@gmail.com>
2024-07-26 10:35:20 +0530

    dynamic content

--- portfolio_website/src/Components/Header.js
+++ portfolio_website/src/Components/Header.js
@@ -12,7 +12,7 @@ export default function Header(){
             <ul className="flex text-white text-xl ">
               <li><a href="/">Home</a></li>
               <li><a href="#about">About</a></li>
-              <li><a href="#project">Project</a></li>
+              <li><a href="#projects">Project</a></li>
               <li><a href="#resume">Resume</a></li>
               <li><a href="#contact">Contact</a></li>
             </ul>
@@ -24,7 +24,7 @@ export default function Header(){
             <ul onClick={()=>setToggleMenu(!toggleMenu)} className="flex flex-col text-white text-xl mobile-nav">
               <li><a href="/">Home</a></li>
               <li><a href="#about">About</a></li>
-              <li><a href="#project">Project</a></li>
+              <li><a href="#projects">Project</a></li>
               <li><a href="#contact">Contact</a></li>
             </ul>
 
--- portfolio_website/src/Components/Projects.js
+++ portfolio_website/src/Components/Projects.js
@@ -3,8 +3,31 @@ import websiteImg2 from "../assets/food-ecommerce.jpg";
 import websiteImg3 from "../assets/website-blog.jpg";
 
 export default function Projects() {
+
+  const config={
+    projects:[
+      {
+        image: websiteImg1,
+        description:"A Ecommerce Website.Built with MERN Stack",
+        link:""
+      },
+      {
+        image: websiteImg2,
+        description:"Food Ecommerce Website like Swiggy,Built with Angular",
+        link:""
+      },
+      {
+        image: websiteImg3,
+        description:"Basic Blog Website.Built with Next Js and MongoDB",
+        link:""
+      }
+      
+    ]
+  }
+
+
   return (
-    <section id="project" className="flex flex-col py-20 px-5 justify-center bg-primary-light text-white">
+    <section id="projects" className="flex flex-col py-20 px-5 justify-center bg-primary-light text-white">
       <div className="w-full ">
         <div className="flex  flex-col px-10 py-5 ">
           <h1 className="text-4xl text-black border-b-4 border-white mb-5 w-[170px] font-bold ">
@@ -18,27 +41,20 @@ export default function Projects() {
 
       <div className="w-full">
         <div className="flex flex-col md:flex-row px-10  gap-5">
-          <div className="relative ">
-            <img className="h-[200px] w-[500px]" src={websiteImg1} />
+
+          {config.projects.map((project)=>(
+            <div className="relative ">  
+            <img className="h-[200px] w-[500px]" src={project.image} />
             <div className="project-desc">
-              <p className="text-center px-5 py-5">A Ecommerce Website.Built with MERN Stack</p>
+              <p className="text-center px-5 py-5">{project.description}</p>
             </div>
 
           </div>
-          <div className="relative">
-            <img className="h-[200px] w-[500px]" src={websiteImg2} />
-            <div className="project-desc">
-                <p className="text-center px-5 py-5 ">Food Ecommerce Website like Swiggy,Built with Angular</p>
-            </div>
 
-          </div>
-          <div className="relative">
-            <img className="h-[200px] w-[500px]" src={websiteImg3} />
-            <div className="project-desc">
-                <p className="text-center px-5 py-5">Basic Blog Website.Built with Next Js and MongoDB</p>
-            </div>
-
-          </div>
+          ))}
+        
+          
+        
           
         
        
@@ -50,26 +66,3 @@ export default function Projects() {
   );
 }
 
-{/* <div className="relative"> 
-            <img className="h-[200px]" src={websiteImg1} />
-
-            <div className="absolute left-0 right-0 bottom-0">
-              <p>A Ecommerce Website.Built with MERN Stack</p>
-            </div>
-
-            <div>
-              <img className="h-[200px]" src={websiteImg2} />
-
-              <div>
-                <p>Food Ecommerce Website like Swiggy,Built with Angular</p>
-              </div>
-            </div>
-
-            <div>
-              <img className="h-[200px]" src={websiteImg3} />
-
-              <div>
-                <p>Basic Blog Website.Built with Next Js and MongoDB</p>
-              </div>
-            </div>
-          </div> */}
