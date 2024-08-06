dac6049982f1f1e7a50da04e1f5a663d4bafd1c4
keerthanamurali612 <keerthanamurali099@gmail.com>
2024-08-06 08:51:41 +0530

    update

--- portfolio_website/src/Components/Header.js
+++ portfolio_website/src/Components/Header.js
@@ -9,12 +9,12 @@ export default function Header(){
         <a className="font-bold text-black text-xl " href="#"> <span className='text-3xl'> P</span>orfolio</a>
 
         <nav className="hidden md:block " >
-            <ul className="flex text-white text-xl   group-hover:w-full transition-all duration-500  ">
-              <li><a href="/">Home</a></li>
-              <li><a href="#about">About</a></li>
-              <li><a href="#projects">Project</a></li>
-              <li><a href="#resume">Resume</a></li>
-              <li><a href="#contact">Contact</a></li>
+            <ul className="flex text-white text-xl  ">
+              <li className='nav-bar '><a  href="/">Home</a></li>
+              <li className='nav-bar '><a href="#about">About</a></li>
+              <li className='nav-bar '><a href="#projects">Project</a></li>
+              <li className='nav-bar '><a href="#resume">Resume</a></li>
+              <li className='nav-bar '><a href="#contact">Contact</a></li>
             </ul>
 
         </nav>
