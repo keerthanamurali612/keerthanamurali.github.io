67e6e6259c587949e7967d6066680b18061aef90
keerthanamurali612 <keerthanamurali099@gmail.com>
2024-09-01 19:36:56 +0530

    update

--- portfolio_website/src/Components/Contact.js
+++ portfolio_website/src/Components/Contact.js
@@ -6,26 +6,27 @@ export default function Contact() {
     phone:'123456789'
   }
     return (
-    
-      
-       
-
+  
 <section id="skills" class=" py-12 ">
   <div class="container mx-auto ">
     <h2 class="text-3xl font-bold text-center mb-8 text-gray-800">Contact</h2>
     <div class=" flex justify-center items-center">
       <div class="bg-white p-6 rounded-lg shadow-md ">
-      <form>
-        <label>Email</label>
-          <input id="email" type="email" required />
 
-        <label>Message</label>
-          <textarea id="message" />
+        <div className=" flex flex-col items-center">        
+            {/* <h1 className="text-4xl text-black border-b-4 border-[#971b0a] mb-5 w-[150px] font-bold ">
+              Contact
+            </h1> */}
+            <p className="2xl pb-5 ">
+              Get in touch             
+            </p>
+            <p className="py-2"><span className="font-bold">Email:</span>{config.email}</p>
+            <p className="py-2"><span className="font-bold">Phone:</span>{config.phone}</p>          
+        </div>
 
-        <button type="submit">Send</button>
-     </form>
-        
-        
+
+
+
       </div>
     </div>
   </div>
