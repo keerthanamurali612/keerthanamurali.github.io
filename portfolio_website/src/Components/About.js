3ee6ff0cf8ae8354a811787e033b3a83d5cbd4bd
keerthanamurali612 <keerthanamurali099@gmail.com>
2024-10-12 18:44:57 +0530

    update

--- portfolio_website/src/Components/About.js
+++ portfolio_website/src/Components/About.js
@@ -4,11 +4,13 @@ import { IoCloudDownloadOutline } from "react-icons/io5";
 export default function About() {
   const config = {
     line1:
-      "Hi,My name is Keerthana Murali .I'm a Front-end developer.I built beautiful Website with Reactjs and Tailwind CSS",
+      "Hello! I’m Keerthana Murali, a passionate Front-End Developer with a Master’s degree in Computer Applications (2023) from Anjalai Ammal Mahalingam Engineering College, India. I specialize in creating elegant and responsive web applications that focus on both functionality and design.",
     line2:
-      "I'm proficient in Front-end skills like Reactjs,Tailwind,CSS,HTML,JS and many more  ",
-    line3: "In backend i know Python,SQL",
-
+      "My expertise lies in React.js, Tailwind CSS, HTML, CSS, and JavaScript, which allows me to craft modern, functional websites. I have also gained experience in Python and SQL for backend development, enabling me to contribute to full-stack projects when needed.",
+    line3: 
+    "I have worked on real-world projects, including developing a web-based application using a combination of front-end and back-end technologies. One of my proudest achievements was collaborating on a project for a charity organization, which received positive feedback for its design and functionality.",
+    line4:
+    " In addition to coding, I enjoy working in dynamic teams and learning new tools and frameworks to keep up with industry trends. I’m always excited to take on new challenges and explore how technology can positively impact people’s lives.",
     link1:""
 
     
@@ -31,6 +33,7 @@ export default function About() {
           <p className="2xl pb-5 font-hero-font "> {config.line1} </p>
           <p className="2xl pb-5 font-hero-font"> {config.line2}</p>
           <p className="2xl pb-5 font-hero-font"> {config.line3} </p>
+          <p className="2xl pb-5 font-hero-font"> {config.line4} </p>
 
 
           <div class="md:w-1/2  flex items-center justify-between  ">
